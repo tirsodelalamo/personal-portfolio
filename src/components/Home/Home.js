e9052a36d9228eb1aa0170c226a22e7b534a8798
Tirso del Álamo <tirsodelalamomartin@gmail.com>
2021-03-30 10:10:07 +0200

    Sidebar completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,12 @@ import data from "../../resumeData.json";
 
 // import Earth from '../UI/Earth/Earth'
 
-// import Typewriter from "typewriter-effect";
-
 const Home = () => {
   console.log(data)
 
   return (
     <>
-      <div className="home">
+      <div className="home" id="Home">
       <div className='intro-page'></div>
       {/* <Earth /> */}
       <div className='intro-text'>
@@ -19,25 +17,6 @@ const Home = () => {
         <h1>and this is my website.</h1>
         <h2>I'm a Full Stack Web Developer</h2>
       </div>
-      {/* <Typewriter
-
-       options={{
-       delay: 50,
-       loop: false
-       }}
-  
-       onInit={(typewriter)=> {
-  
-       typewriter
-       
-       .typeString('<span class="a" >H</span><span class="a" >i</span><span class="a" >,</span>') 
-       .pauseFor(500)
-       .typeString('<br><span class="a" >I</span><span class="a" >m</span> <span class="a" >T</span><span class="a" >i</span><span class="a" >r</span><span class="a" >s</span><span class="a" >o</span><span class="a" >,</span>') 
-       .pauseFor(500)
-       .typeString('<br><span class="a" >W</span><span class="a" >e</span><span class="a" >b</span> <span class="a" >d</span><span class="a" >e</span><span class="a" >v</span><span class="a" >e</span><span class="a" >l</span><span class="a" >o</span><span class="a" >p</span><span class="a" >e</span><span class="a" >r</span>')
-       .start();
-       }}
-       /> */}
     </div>
     </>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { bool } from 'prop-types';
 import { StyledMenu } from './Navbar.styled';
 
-const Menu = ({ open }) => {
+const Menu = ({ open, setOpen }) => {
   return (
     <StyledMenu open={open}>
-      <a href="#home">
+      <a href="#Home" open={open} onClick={() => setOpen(!open)}>
           <span role="img" aria-label="home"></span>
           Home
         </a>
-        <a href="#About">
+        <a href="#About" open={open} onClick={() => setOpen(!open)}>
           <span role="img" aria-label="about"></span>
           About
         </a>
-        <a href="#Skills">
+        <a href="#Skills" open={open} onClick={() => setOpen(!open)}>
           <span role="img" aria-label="skills"></span>
           Skills
         </a>
-        <a href="#Projects">
+        <a href="#Projects" open={open} onClick={() => setOpen(!open)}>
           <span role="img" aria-label="projects"></span>
           Projects
         </a>
-        <a href="#Contact">
+        <a href="#Contact" open={open} onClick={() => setOpen(!open)}>
           <span role="img" aria-label="contact"></span>
           Contact
         </a>
